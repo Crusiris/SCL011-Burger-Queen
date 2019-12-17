c4a829857bf2622c3a98a81c3ce2e6f8593c6ff8
macarenasaez <makadaira@gmail.com>
2019-12-17 13:03:51 -0300

    routing pedidosComponent

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -8,9 +8,10 @@ import { PedidosComponent } from "../app/components/pedidos/pedidos.component";
 
 const ROUTES: Routes = [
 { path: 'home', component:startScreenComponent },
+{ path: 'pedidos', component: PedidosComponent},
 { path: 'menu', component:MenuComponent },
 { path: '**', pathMatch: 'full', redirectTo: 'home' },
-{ path: 'pedidos', component: PedidosComponent}
+
 
 ];
 @NgModule({
